003ee736e0037b669888f47e3d73f2b37f788b04
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-06-03 18:35:20 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1251,7 +1251,7 @@ def xpshort(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://m.freshpagal.in/"
+    ref = "https://pro.finsurances.co/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
