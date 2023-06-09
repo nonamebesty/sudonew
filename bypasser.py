2d029d4372685ce65f8fae516dba74a29b14a8a5
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-10 02:34:55 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1838,7 +1838,7 @@ def seturl(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "shortner.mphealth.online/"
+    ref = "mphealth.online/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
