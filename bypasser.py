44d98b3b3fdc2983bfd0b8d2cbc0fd4acd5d9211
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-10 02:02:42 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1375,29 +1375,7 @@ def oggylink(url):
 
 
 ##########################################################################
-# krownlinks
 
-
-def krownlinks(url):
-    client = requests.session()
-    DOMAIN = "https://tech.bloggertheme.xyz"
-    url = url[:-1] if url[-1] == "/" else url
-    code = url.split("/")[-1]
-    final_url = f"{DOMAIN}/{code}"
-    resp = client.get(final_url)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    try:
-        inputs = soup.find(id="go-link").find_all(name="input")
-    except BaseException:
-        return "Incorrect Link"
-    data = {input.get("name"): input.get("value") for input in inputs}
-    h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(10)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()["url"]
-    except BaseException:
-        return "Something went wrong :("
 
 
 ##########################################################################
@@ -1998,11 +1976,6 @@ def shortners(url):
     elif "https://ez4short.com/" in url:
         print("entered ez4short:", url)
         return ez4(url)
-
-    # krownlinks
-    elif "https://krownlinks.me/" in url:
-        print("entered krownlinks:", url)
-        return krownlinks(url)
 
     # adrinolink
     elif "https://adrinolinks." in url:
