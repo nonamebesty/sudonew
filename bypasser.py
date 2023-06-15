4f145b1220a52fc3a077d8f188ad8a2b061569ca
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-15 23:34:44 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1880,7 +1880,7 @@ def tnshortnet(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "http://financeyogi.net/"
+    ref = "https://market.finclub.in/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
