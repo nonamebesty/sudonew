30ca666b589fd9405d4262545d6949df393e703b
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-17 23:19:22 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1779,7 +1779,7 @@ def tnvalue(url):
     inputs = soup.find_all("input")
     data = {input.get("name"): input.get("value") for input in inputs}
     h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(9)
+    time.sleep(12)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return str(r.json()["url"])
