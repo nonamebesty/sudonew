6a4a38c7a5754b8217de9c3e61991b49c5868011
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-17 23:46:52 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1935,25 +1935,25 @@ def dalink(url):
     except BaseException:
         return "Something went wrong :("
 
-def onepagelinkin(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://go.onepagelink.in/"
-    url = url[:-1] if url[-1] == "/" else url
-    code = url.split("/")[-1]
-    final_url = f"{DOMAIN}/{code}"
-    ref = "https://gorating.in"
-    h = {"referer": ref}
-    response = client.get(final_url, headers=h)
-    soup = BeautifulSoup(response.text, "html.parser")
-    inputs = soup.find_all("input")
-    data = {input.get("name"): input.get("value") for input in inputs}
-    h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(9)
-    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return r.json()["url"]
-    except BaseException:
-        return "Something went wrong :("
+#def onepagelinkin(url):
+#    client = cloudscraper.create_scraper(allow_brotli=False)
+#    DOMAIN = "https://go.onepagelink.in/"
+#    url = url[:-1] if url[-1] == "/" else url
+#    code = url.split("/")[-1]
+#    final_url = f"{DOMAIN}/{code}"
+#    ref = "https://gorating.in/"
+#    h = {"referer": ref}
+#    response = client.get(final_url, headers=h)
+#    soup = BeautifulSoup(response.text, "html.parser")
+#    inputs = soup.find_all("input")
+#    data = {input.get("name"): input.get("value") for input in inputs}
+#    h = {"x-requested-with": "XMLHttpRequest"}
+#    time.sleep(9)
+#    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+#    try:
+#        return r.json()["url"]
+#    except BaseException:
+#        return "Something went wrong :("
 
 #Jai Add Later
 ##########################################################################
