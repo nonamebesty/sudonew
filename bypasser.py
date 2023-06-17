b9a4eac6b1cb951ba2f0a78c1e2d0d3a75c69f20
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-06-17 22:45:10 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1914,6 +1914,27 @@ def tnshortnet(url):
     except BaseException:
         return "Something went wrong :("
 
+
+def dalink(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://dlx.tamilhit.tech"
+    url = url[:-1] if url[-1] == "/" else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://tamilhit.tech"
+    h = {"referer": ref}
+    response = client.get(final_url, headers=h)
+    soup = BeautifulSoup(response.text, "html.parser")
+    inputs = soup.find_all("input")
+    data = {input.get("name"): input.get("value") for input in inputs}
+    h = {"x-requested-with": "XMLHttpRequest"}
+    time.sleep(9)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()["url"]
+    except BaseException:
+        return "Something went wrong :("
+
 #Jai Add Later
 ##########################################################################
 # helpers
@@ -2133,6 +2154,10 @@ def shortners(url):
     elif "https://greylinks.in/" in url or "https://go.greymatterslinks.in/" in url:
         print("entered greylink:", url)
         return greylink(url)
+
+elif "https://dalink.in/" in url or "https://dlx.tamilhit.tech/" in url:
+        print("entered dalink:", url)
+        return dalink(url)
     
     elif "https://link1s.com/" in url:
         print("entered link1s:", url)
