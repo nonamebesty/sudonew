0f3be7e039271e31c9d5b7b62d2e7bd0c252a395
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-07-15 23:42:35 +0530

    Update ddl.py

--- ddl.py
+++ ddl.py
@@ -618,28 +618,50 @@ def uploadee(url: str) -> str:
 
 
 def terabox(url) -> str:
-    #if not path.isfile("terabox.txt"):
-        #raise DirectDownloadLinkException("ERROR: terabox.txt not found")
-    session = create_scraper()
-    try:
-        res = session.request("GET", url)
-        key = res.url.split("?surl=")[-1]
-        jar = MozillaCookieJar("terabox.txt")
-        jar.load()
-        session.cookies.update(jar)
-        res = session.request(
-            "GET",
-            f"https://www.terabox.com/share/list?app_id=250528&shorturl={key}&root=1",
-        )
-        result = res.json()["list"]
-    except Exception as e:
-        raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}")
-    if len(result) > 1:
-        raise DirectDownloadLinkException("ERROR: Can't download mutiple files")
+    sess = session()
+    while True:
+          try: 
+              res = sess.get(url)
+              print("connected")
+              break
+          except: print("retrying")
+    url = res.url
+
+    key = url.split('?surl=')[-1]
+    url = f'http://www.terabox.com/wap/share/filelist?surl={key}'
+    sess.cookies.update(TERA_COOKIE)
+
+    while True:
+        try: 
+            res = sess.get(url)
+            print("connected")
+            break
+        except Exception as e: print("retrying")
+
+    key = res.url.split('?surl=')[-1]
+    soup = BeautifulSoup(res.content, 'lxml')
+    jsToken = None
+
+    for fs in soup.find_all('script'):
+        fstring = fs.string
+        if fstring and fstring.startswith('try {eval(decodeURIComponent'):
+            jsToken = fstring.split('%22')[1]
+
+    while True:
+        try:
+            res = sess.get(f'https://www.terabox.com/share/list?app_id=250528&jsToken={jsToken}&shorturl={key}&root=1')
+            print("connected")
+            break
+        except: print("retrying")
+    result = res.json()
+
+    if result['errno'] != 0: return f"ERROR: '{result['errmsg']}' Check cookies"
+    result = result['list']
+    if len(result) > 1: return "ERROR: Can't download mutiple files"
     result = result[0]
-    if result["isdir"] != "0":
-        raise DirectDownloadLinkException("ERROR: Can't download folder")
-    return result["dlink"]
+
+    if result['isdir'] != '0':return "ERROR: Can't download folder"
+    return result.get('dlink',"Error")
 
 
 
