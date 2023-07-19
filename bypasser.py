d445b5d03ae0d7393bc2c05f27efeb73be117ce0
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-07-19 22:13:22 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1756,7 +1756,7 @@ def v2kpslink(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://prakasheditingtamizhan.com/"
+    ref = "https://djusmusic.in/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
