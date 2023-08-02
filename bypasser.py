22f6da80a50723b51c869bb8eb678adb214d328f
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-08-03 00:32:59 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2010,25 +2010,40 @@ def pkinme(url):
 
 
 # tnshort.net
-def tnshortnet(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)
+#def tnshortnet(url):
+   # client = cloudscraper.create_scraper(allow_brotli=False)
+    #DOMAIN = "https://news.speedynews.xyz/"
+    #url = url[:-1] if url[-1] == "/" else url
+    #code = url.split("/")[-1]
+    #final_url = f"{DOMAIN}/{code}"
+    #ref = "https://market.finclub.in/"
+    #h = {"referer": ref}
+    #resp = client.get(final_url, headers=h)
+    #soup = BeautifulSoup(resp.content, "html.parser")
+    #inputs = soup.find_all("input")
+    #data = {input.get("name"): input.get("value") for input in inputs}
+    #h = {"x-requested-with": "XMLHttpRequest"}
+    #time.sleep(13)
+    #r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    #try:
+        #return str(r.json()["url"])
+    #except BaseException:
+        #return "Something went wrong :("
+
+def tnshort(url):
     DOMAIN = "https://news.speedynews.xyz/"
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://market.finclub.in/"
-    h = {"referer": ref}
-    resp = client.get(final_url, headers=h)
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    resp = client.get(final_url)
     soup = BeautifulSoup(resp.content, "html.parser")
-    inputs = soup.find_all("input")
+    inputs = soup.find(id="go-link").find_all(name="input")
     data = {input.get("name"): input.get("value") for input in inputs}
     h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(13)
+    time.sleep(12)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try:
-        return str(r.json()["url"])
-    except BaseException:
-        return "Something went wrong :("
+    return r.json()["url"]
 
 
 def dalink(url):
@@ -2636,7 +2651,7 @@ def shortners(url):
         return linkbnao(url)
 
     # tnshort
-    elif "tnshort.in" in url:
+    elif "https://link.tnshort.net/" in url:
         print("entered tnshort:", url)
         return tnshort(url)
 
