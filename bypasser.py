4e9d74aac1191e93fccc82236167c7934438ec9e
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-08-10 19:55:34 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2043,7 +2043,7 @@ def tnshort(url):
 
 def dalink(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://www.tamilhit.tech/"
+    DOMAIN = "https://www.tamilhit.tech/blog//"
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
