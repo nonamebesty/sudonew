9a2b8f891e80afa8023c4f382f7604d7727390ee
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-09-09 20:09:40 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1286,9 +1286,9 @@ def xpshort(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    #ref = "https://m.awmnews.in/"
+    ref = "https://www.newspadj.in/"
     #ref = "https://www.twinthrottlers.xyz/"
-    ref = "https://www.animalwallpapers.online/"
+    #ref = "https://www.animalwallpapers.online/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
