4770751c064553e69d2142cc4008750b1b563b1d
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-09-15 14:05:59 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1951,11 +1951,11 @@ def tamizhmasters(url):
 
 def krownlinks(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://earn.hostadviser.net/"
+    DOMAIN = "https://go.hostadviser.net/"
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://hostadviser.net/"
+    ref = "https://www.gyanitheme.com/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
