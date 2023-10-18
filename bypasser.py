b73615bca96e5d98b4a0b3b43c9c598c2dba9f42
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-10-19 01:36:43 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2112,7 +2112,7 @@ def onepagelinkin(url):
 
 def zxlink(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://zxlink.in/"
+    DOMAIN = "https://zxlink.in"
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
@@ -2123,7 +2123,7 @@ def zxlink(url):
     inputs = soup.find_all("input")
     data = {input.get("name"): input.get("value") for input in inputs}
     h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(9)
+    time.sleep(5)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return r.json()["url"]
