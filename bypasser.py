2dcba348650fe8c7499f880588b0f8cd1fa8335a
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-12-09 00:00:57 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2066,6 +2066,27 @@ def tnshort(url):
     except BaseException:
         return "Something went wrong :("
 
+ #mdisky
+def mdisky(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://go.bloggingaro.com/"
+    url = url[:-1] if url[-1] == "/" else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://marioworlds.bloggingaro.com/"
+    h = {"referer": ref}
+    resp = client.get(final_url, headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = {input.get("name"): input.get("value") for input in inputs}
+    h = {"x-requested-with": "XMLHttpRequest"}
+    time.sleep(6)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return str(r.json()["url"])
+    except BaseException:
+        return "Something went wrong :("
+
 def link2paisa(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
     DOMAIN = "https://go.link2paisa.com/"
@@ -2843,6 +2864,10 @@ def shortners(url):
     elif "https://link.tnshort.net/" in url:
         print("entered tnshort:", url)
         return tnshort(url)
+
+    elif "mdisky.link" in url:
+        print("entered mdisky:", url)
+        return mdisky(url)
 
     elif "https://link2paisa.com/" in url:
         print("entered link2paisa:", url)
