906ff6a878c4f8996f942bfcbf8ad5510381a00d
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-05-26 01:18:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,18 +18,17 @@ from scraper import scrapper, scrapper_sites
 from texts import HELP_TEXT
 
 # bot
-bot_token = os.environ.get("TOKEN", "")
-api_hash = os.environ.get("HASH", "") 
-api_id = os.environ.get("ID", "")
-OWNER_ID = os.environ.get("OWNER_ID", "")
+bot_token = os.environ.get("TOKEN", "6665032973:AAEoRsnrvaAr0Vn3bgrizVaMjezo2Stlh7I")
+api_hash = os.environ.get("HASH", "fcdc178451cd234e63faefd38895c991") 
+api_id = os.environ.get("ID", "1923471")
+OWNER_ID = os.environ.get("OWNER_ID","880087645")
 ADMIN_LIST = [int(ch) for ch in (os.environ.get("ADMIN_LIST", f"{OWNER_ID}")).split()]
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
-PERMANENT_GROUP = os.environ.get("PERMANENT_GROUP", "")
+OWNER_USERNAME = "JAsuran2p0"
+PERMANENT_GROUP = os.environ.get("PERMANENT_GROUP", "-1002099304315")
 GROUP_ID = [int(ch) for ch in (os.environ.get("GROUP_ID", f"{PERMANENT_GROUP}")).split()]
-UPDATES_CHANNEL = str(os.environ.get("UPDATES_CHANNEL", ""))
-DB_URL = os.environ.get("DB_URL", "")
-U_NAME = os.environ.get("BOT_USERNAME", "")
-
+UPDATES_CHANNEL = str(os.environ.get("UPDATES_CHANNEL"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://dhanapal:dhanapal@dhanapal.pvrwtmv.mongodb.net/?retryWrites=true&w=majority")
+U_NAME = os.environ.get("BOT_USERNAME", "ASLinkBypasserBOt")
 
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)
 
